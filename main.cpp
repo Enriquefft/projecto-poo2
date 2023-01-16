67309c18c5a63daf75b9d7606e12a7a9550e8846
Enriquefft <enriquefft2001@gmail.com>
2023-01-16 18:50:16 -0500

    simpler main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,25 @@
-/*******************************************************************************************
- *
- *   raylib-cpp [core] example - Basic window (adapted for HTML5 platform)
- *
- *   This example is prepared to compile for PLATFORM_WEB, PLATFORM_DESKTOP and
- *PLATFORM_RPI As you will notice, code structure is slightly diferent to the
- *other examples... To compile it for PLATFORM_WEB just uncomment #define
- *PLATFORM_WEB at beginning
- *
- *   This  has been created using raylib-cpp (www.raylib.com)
- *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h
- *for details)
- *
- *   Copyright (c) 2015 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include "raylib-cpp.hpp"
 
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
-int screenWidth = 800;
-int screenHeight = 450;
-
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void); // Update and Draw one frame
-
-//----------------------------------------------------------------------------------
-// Main Enry Point
-//----------------------------------------------------------------------------------
 int main() {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  raylib::Window window(screenWidth, screenHeight,
-                        "raylib-cpp [core] example - basic window");
+  int screenWidth = 800;
+  int screenHeight = 450;
 
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
+  raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
 
-  // Main game loop
-  while (!window.ShouldClose()) // Detect window close button or ESC key
-  {
-    UpdateDrawFrame();
+  SetTargetFPS(60);
+
+  while (!window.ShouldClose()) {
+    BeginDrawing();
+
+    window.ClearBackground(RAYWHITE);
+
+    DrawText("Congrats! You created your first window!", 190, 200, 20,
+             LIGHTGRAY);
+
+    EndDrawing();
   }
 
+  // UnloadTexture() and CloseWindow() are called automatically.
+
   return 0;
-}
-
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void) {
-  // Update
-  //----------------------------------------------------------------------------------
-  // TODO: Update your variables here
-  //----------------------------------------------------------------------------------
-
-  // Draw
-  //----------------------------------------------------------------------------------
-  BeginDrawing();
-
-  ClearBackground(RAYWHITE);
-
-  DrawText("Congrats! You created your first raylib-cpp window!", 190, 200, 20,
-           LIGHTGRAY);
-
-  EndDrawing();
-  //----------------------------------------------------------------------------------
 }
