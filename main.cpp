5b6154afded10b570f4251653feb3d315f655ba6
Enriquefft <enriquefft2001@gmail.com>
2023-01-18 20:31:37 -0500

    removed warnings for headers

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
+#pragma GCC diagnostic push
+#pragma clang diagnostic push
+#pragma clang attribute push(__attribute__((no_sanitize("undefined"))),        \
+                             apply_to = function)
+
 #pragma GCC diagnostic ignored "-Weverything"
-#pragma clang diagnostic ignored "-Wpadded"
+#pragma clang diagnostic ignored "-Weverything"
+
 #include "raylib-cpp.hpp"
-#pragma GCC diagnostic on
-#pragma clang diagnostic on
+#pragma GCC diagnostic pop
+#pragma clang diagnostic pop
+#pragma clang attribute pop
+
 #include <iostream>
+
+struct Padded {
+  int i;
+  bool b;
+  float f;
+};
 
 int main() {
   int screenWidth = 800;
   int screenHeight = 450;
+  constexpr auto was_padded =
+      sizeof(Padded) != sizeof(int) + sizeof(bool) + sizeof(float);
 
   raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
 
+  int x = 3;
   SetTargetFPS(60);
 
   while (!window.ShouldClose()) {
