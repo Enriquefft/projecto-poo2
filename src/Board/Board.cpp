58ead800f213342a200ab9eec0a696e2f30a05fa
Enriquefft <enriquefft2001@gmail.com>
2023-01-24 10:40:28 -0500

    minor changes

--- src/Board/Board.cpp
+++ src/Board/Board.cpp
@@ -10,5 +10,7 @@ constexpr uint8_t MIN_HEIGHT = 15;
 Board::Board() {
   auto height = Utils::RandomNum<uint8_t>(MIN_HEIGHT, MAX_HEIGHT);
   uint8_t width = Utils::RandomNum<uint8_t>(MAX_WIDTH, MAX_WIDTH);
+
+  // Generate random maze using kruskal's random algorithm (random weights)
 }
 Board::Board(uint8_t height, uint8_t width) {}
